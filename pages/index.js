7aa464ffa7bb7f9924f7274177d5c7d5fcde5d36
Nikruto <nikruto1456@gmail.com>
2020-11-02 13:37:01 +0300

    update title

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Yayın var mı?</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
